c3ad665938abcbccd2313bf807430b3b6ce29a52
josephesario <joserodolfoesapa@gmail.com>
2023-11-22 16:56:13 +0000

    # Commit: Design the Model Helper # This design enhances our ability to gather well-structured JSON data. # By implementing this model helper, we ensure a more organized and effective JSON data collection process.

---
+++ Helper/HModels/HBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public partial class HBook
+{
+    [Key]
+    [Column("Book_Id")]
+    [Required]
+    public Guid BookId { get; set; }
+
+    [Required]
+    [StringLength(120)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [Column("Phone_Number")]
+    [StringLength(50)]
+    public string PhoneNumber { get; set; } = null!;
+
+}
