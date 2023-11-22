334c0d9092cb2cdaac853647ef78ac5b21ebaa66
josephesario <joserodolfoesapa@gmail.com>
2023-11-22 16:51:00 +0000

    #Project Structure #Main project  -- PhoneBookApp #Followed By three Libraries #dbContex Library #Helper Library #Interfaces Library #Secure Library for all validations token etc
    #Generating the dbContext and moving the connection string to the main project Json file where I do all the configurations.
    #Json file name -- appsettings.json

---
+++ Model/Models/PhoneBookDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace dbContext.Models;
+
+public partial class PhoneBookDbContext : DbContext
+{
+    public PhoneBookDbContext()
+    {
+    }
+
+    public PhoneBookDbContext(DbContextOptions<PhoneBookDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string? connectionString = configuration.GetConnectionString("PhoneBookDbContext");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
